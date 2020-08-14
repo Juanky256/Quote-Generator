6f28e75aae08af9843c4fa08dab2720392e5e6ac
Juanky256 <juanky006@gmail.com>
2020-08-14 06:59:45 -0500

    Improvin functionality

--- script.js
+++ script.js
@@ -3,20 +3,63 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+//Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loader
+function Loaded() {
+    if (!loader.hidden) {
+        loader.hidden = true;
+        quoteContainer.hidden = false;
+    }
+}
 
 //Get Quote From API
 async function getQuote() {
-    const proxyUrl = "https://agile-garden-81942.herokuapp.com/"
-    const apiUrl = "https://api.forismatic.com/api/1.0?method=getQuote&lang=en&format=json";
+    loading();
+    const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        authorText.innerText = data.quoteAuthor;
+        //If Author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = "Unknown";
+        }
+        else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        //Reduce font size for quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add("long-quote");
+        }
+        else {
+            quoteText.classList.remove("long-quote");
+        }
         quoteText.innerText = data.quoteText;
-    } catch (error) {
-    
+        Loaded();
+    } 
+    catch (error) {
+        console.log(error);
+        getQuote();
     }
 }
+
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 //On Load
 getQuote();
