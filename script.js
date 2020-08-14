79f3eba0d4f6c8dc3f5f75f54249c3aebf2bf734
Juanky256 <juanky006@gmail.com>
2020-08-14 07:24:35 -0500

    CORS change

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function Loaded() {
 //Get Quote From API
 async function getQuote() {
     loading();
-    const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+    const proxyUrl = 'https://sleepy-gorge-63535.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
